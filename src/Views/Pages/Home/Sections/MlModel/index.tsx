03876f4b0d353fb75f1af3f0d626c8fa0a480803
Aryan Sadeghi <aryan.sadeghi@snapp.cab>
2023-08-04 05:24:05 +0330

    update streamlit frame

--- src/Views/Pages/Home/Sections/MlModel/index.tsx
+++ src/Views/Pages/Home/Sections/MlModel/index.tsx
@@ -8,7 +8,7 @@ const MlModel = () => {
 		<div className='contact-us' id='contact-us' >
 			<h2 className='mb-1' >Machine Learning Models</h2>
 				<iframe id="serviceFrameSend"
-						style={{height: '600px', width:'100%'}}	src="http://178.128.236.200:8501/"
+						style={{height: '800px', width:'100%'}}	src="http://178.128.236.200:8501/"
 						frameBorder="0"></iframe>
 
 		</div>
