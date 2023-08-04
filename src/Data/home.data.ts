01bf4dfbea586246272e26d76157a0bab10d4cf6
Aryan Sadeghi <aryan.sadeghi@snapp.cab>
2023-08-04 19:45:52 +0330

    update charts, add bi gram.

--- src/Data/home.data.ts
+++ src/Data/home.data.ts
@@ -9,95 +9,103 @@ import chart2 from '@assets/Images/home/chart2.jpg';
 import chart3 from '@assets/Images/home/chart3.jpg';
 
 export const our_vision = {
-	section: ' ',
-	title: 'DECODING SCHOOL DROPOUT PREDICTION',
-	sub_title: ' ',
-	description:
-		'this project aims to use the Machine Learning technology to predict dropout rate based on various features, including but not limited to health information, \n' +
-		'    student demographic and school background, to help different organization to have a handy but reliable dropout systems and  platform.',
+    section: ' ',
+    title: 'DECODING SCHOOL DROPOUT PREDICTION',
+    sub_title: ' ',
+    description:
+        'this project aims to use the Machine Learning technology to predict dropout rate based on various features, including but not limited to health information, \n' +
+        '    student demographic and school background, to help different organization to have a handy but reliable dropout systems and  platform.',
 };
 
 export const our_chart = [
-	{
-		section: 'Chord Diagram',
-		title: 'Dropout Risk Factors',
-		description:
-		'This chord diagram visualizes the frequency of co-occurrence between different risk factors. There are 30 risk factors categorized from features extracted in previous studies. The width of the ribbons connecting two risk factors corresponds to the number of countries where those risk factors are used together in building predictive models. By examining the connections between risk factors, we can identify the most significant patterns and correlations that could aid in selecting predictive features.',
-		iFrameLink : 'chord_Category_risk.html',
-		frameInNewRow: false,
-	},
-	{
-		section: 'Sunburst',
-		frameInNewRow: false,
-		title: '',
-		description:
-	'This Sunburst diagram contains the first two levels of the hierarchical structure, revealing the relationships between dimensions and risk factors. Each segment\'s size represents the frequency of its appearance in predictive models, enabling you to identify significant categories quickly.',
-		iFrameLink : 'sunburst_FreqCounts_withoutFeature.html',
-	},	{
-		section: 'Collapsible Tree',
-		title: '',
-		description:
-	'The Collapsible Tree diagram presents an interactive visualization of feature categorization for predicting student school dropout. The features are grouped into 30 distinct risk factors, and these 30 risk factors are further categorized into 9 comprehensive dimensions. Each feature node displays its frequency of appearance in previous school dropout models, allowing you to identify crucial predictors. You can explore the features by selecting the dimensions and risk factors you are interested in.',
-		iFrameLink : 'collapsibleTree_FreqCounts.html',
-		frameInNewRow: true,
-	},{
-		section: 'Interactive Treemap',
-		title: '',
-		frameInNewRow: true,
-		description:
-'The Treemap visualization showcases three hierarchical levels while displaying one level at a time. By clicking on a rectangle, you can delve deeper into lower-level categories, gaining a better understanding of their hierarchical relationships. The size of each rectangle corresponds to the frequency of its appearance in predictive models, enabling effortless comparisons of feature usage.',
-		iFrameLink : 'interactive_treemap.html',
-	},{
-		section: 'Treemap',
-		title: '',
-		frameInNewRow: true ,
-		description:
-		'Unlike the previous interactive Treemap, this graph showcases all three hierarchical levels simultaneously, providing a comprehensive overview of the relationships between features, risks, and dimensions. The size of each rectangle corresponds to the frequency of its appearance in predictive models. The colour palette further enhances the visualization. By hovering over the rectangles, you can access additional information, such as the specific count for each feature.',
-			iFrameLink : 'treeMap.html',
-	},
+    {
+        section: 'Chord Diagram',
+        title: 'Dropout Risk Factors',
+        description:
+            'This chord diagram visualizes the frequency of co-occurrence between different risk factors. There are 30 risk factors categorized from features extracted in previous studies. The width of the ribbons connecting two risk factors corresponds to the number of countries where those risk factors are used together in building predictive models. By examining the connections between risk factors, we can identify the most significant patterns and correlations that could aid in selecting predictive features.',
+        iFrameLink: 'chord_Category_risk.html',
+        frameInNewRow: false,
+    },
+    {
+        section: 'Bi Gram',
+        title: '',
+        description:
+            'The Bi-gram graph visualizes co-occurrence patterns of 30 risk factors extracted from predictive features for student school dropout. Each node represents a risk factor, and connections indicate how often two risk factors co-occur in predictive models. Adjusting the "min_freq" parameter would help you to focus on more prevalent co-occurrences. This user-friendly tool simplifies the process of selecting relevant features by tuning the \'min_freq\' until the desired patterns emerge.',
+        iFrameLink: 'http://178.128.236.200:8502/',
+        frameInNewRow: false,
+    },
+    {
+        section: 'Sunburst',
+        frameInNewRow: false,
+        title: '',
+        description:
+            'This Sunburst diagram contains the first two levels of the hierarchical structure, revealing the relationships between dimensions and risk factors. Each segment\'s size represents the frequency of its appearance in predictive models, enabling you to identify significant categories quickly.',
+        iFrameLink: 'sunburst.html',
+    }, {
+        section: 'Collapsible Tree',
+        title: '',
+        description:
+            'The Collapsible Tree diagram presents an interactive visualization of feature categorization for predicting student school dropout. The features are grouped into 30 distinct risk factors, and these 30 risk factors are further categorized into 9 comprehensive dimensions. Each feature node displays its frequency of appearance in previous school dropout models, allowing you to identify crucial predictors. You can explore the features by selecting the dimensions and risk factors you are interested in.',
+        iFrameLink: 'collapsibleTree_FreqCounts.html',
+        frameInNewRow: true,
+    }, {
+        section: 'Interactive Treemap',
+        title: '',
+        frameInNewRow: true,
+        description:
+            'The Treemap visualization showcases three hierarchical levels while displaying one level at a time. By clicking on a rectangle, you can delve deeper into lower-level categories, gaining a better understanding of their hierarchical relationships. The size of each rectangle corresponds to the frequency of its appearance in predictive models, enabling effortless comparisons of feature usage.',
+        iFrameLink: 'interactive_treemap.html',
+    }, {
+        section: 'Treemap',
+        title: '',
+        frameInNewRow: true,
+        description:
+            'Unlike the previous interactive Treemap, this graph showcases all three hierarchical levels simultaneously, providing a comprehensive overview of the relationships between features, risks, and dimensions. The size of each rectangle corresponds to the frequency of its appearance in predictive models. The colour palette further enhances the visualization. By hovering over the rectangles, you can access additional information, such as the specific count for each feature.',
+        iFrameLink: 'treeMap.html',
+    },
 
 ];
 
 export const instructor = {
-	title: 'Collaborators',
-	people : [
-		{
-			name: 'Zahra Shakeri',
-			image: 'https://hivelab-uoft.ca/images/team/zahra.jpg',
-			description: 'Supervisor',
-		},{
-			name: 'Ziqi Shu',
-			image: 'https://hivelab-uoft.ca/images/team/ziqi_shu.jpeg',
-			description: 'Creator',
-		}
-		,{
-			name: 'Jiayu Liangm',
-			image: 'https://hivelab-uoft.ca/images/team/Grace.JPG',
-			description: 'Creator',
-		},{
-			name: 'Yong Chen',
-			image: 'https://hivelab-uoft.ca/images/team/yong.png',
-			description: 'Creator',
-		}
+    title: 'Collaborators',
+    people: [
+        {
+            name: 'Zahra Shakeri',
+            image: 'https://hivelab-uoft.ca/images/team/zahra.jpg',
+            description: 'Supervisor',
+        }, {
+            name: 'Ziqi Shu',
+            image: 'https://hivelab-uoft.ca/images/team/ziqi_shu.jpeg',
+            description: 'Creator',
+        }
+        , {
+            name: 'Jiayu Liangm',
+            image: 'https://hivelab-uoft.ca/images/team/Grace.JPG',
+            description: 'Creator',
+        }, {
+            name: 'Yong Chen',
+            image: 'https://hivelab-uoft.ca/images/team/yong.png',
+            description: 'Creator',
+        }
 
-	],
-	logos: [toronto, dalla],
+    ],
+    logos: [toronto, dalla],
 };
 
 export const contact_us = {
-	section: 'Get in Touch!',
-	description: `Want to know more or collaborate?
+    section: 'Get in Touch!',
+    description: `Want to know more or collaborate?
 	Get in touch with us below and be part of our data-driven storytelling team. 
 	`,
-	dropdownList: [
-		'Providing Feedback',
-		'Interested to Collaborate',
-		'Need to Access the Course (Select if you are registered for the course)',
-		'Others',
-	],
+    dropdownList: [
+        'Providing Feedback',
+        'Interested to Collaborate',
+        'Need to Access the Course (Select if you are registered for the course)',
+        'Others',
+    ],
 };
 
 export const footer = {
-	description:
-		'If you are interested in collaborating with us or would like to learn more about our reseach projects, please feel free to contact us at zahra.shakeri@utoronto.ca.',
+    description:
+        'If you are interested in collaborating with us or would like to learn more about our reseach projects, please feel free to contact us at zahra.shakeri@utoronto.ca.',
 };
---
+++ streamlit/bi_gram_streamlit.py
@@ -0,0 +1,122 @@
+import io
+import csv
+import networkx as nx
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from itertools import combinations
+from collections import defaultdict
+import streamlit as st
+
+dataset1_file = './dataset_1.csv'
+dataset3_file = './dataset_3.csv'
+
+def load_csv(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+    return data
+
+dataset1 = load_csv(dataset1_file)
+dataset3 = load_csv(dataset3_file)
+
+feature_categories = {}
+for row in dataset3[1:]:
+    feature = row[0]
+    category = row[1]
+    feature_categories[feature] = category
+
+category_names = list(set(feature_categories.values()))
+
+dataset1_categories = []
+for row in dataset1[1:]:
+    categories = [feature_categories[feature] for feature, value in zip(dataset1[0][1:], row[1:]) if
+                  value == '1' and feature in feature_categories]
+    dataset1_categories.append(categories)
+
+n = 2
+min_frequency = 8
+
+graph = nx.Graph()
+
+co_occurrence_counts = defaultdict(lambda: defaultdict(int))
+
+for categories in dataset1_categories:
+    country_categories = set(categories)
+    for combination in combinations(country_categories, n):
+        co_occurrence_counts[combination][tuple(categories)] += 1
+
+for combination, counts_per_country in co_occurrence_counts.items():
+    count = sum(counts_per_country.values())
+    if count >= min_frequency:
+        graph.add_edge(*combination, weight=count)
+
+
+def generate_graph(min_frequency):
+    graph = nx.Graph()
+
+    co_occurrence_counts = defaultdict(lambda: defaultdict(int))
+
+    for categories in dataset1_categories:
+        country_categories = set(categories)
+        for combination in combinations(country_categories, n):
+            co_occurrence_counts[combination][tuple(categories)] += 1
+
+    for combination, counts_per_country in co_occurrence_counts.items():
+        count = sum(counts_per_country.values())
+        if count >= min_frequency:
+            graph.add_edge(*combination, weight=count)
+
+    # plt.figure(figsize=(12, 8))
+    # plt.figure(figsize=(16, 10))  # Set a larger figsize for the graph
+    num_nodes = len(graph.nodes)
+    label_lengths = [len(node) for node in graph.nodes]
+    max_label_length = max(label_lengths)
+    figure_width = max(20, num_nodes * 0.7, max_label_length * 0.7)
+    figure_height = max(15, num_nodes * 0.5, max_label_length * 0.5)
+
+    plt.figure(figsize=(figure_width, figure_height))
+    pos = nx.spring_layout(graph, seed=2017)
+    # pos['Current Study Program'] = (0.1, 0.5)  # Adjust the position of 'current study program'
+    # pos['Previous Academic Performance'] = (0.9, 0.5)  # Adjust the position of 'previous study performance'
+    edge_weights = nx.get_edge_attributes(graph, 'weight')
+    max_edge_weight = max(edge_weights.values())  # Get the maximum edge weight
+
+    nx.draw_networkx_edges(graph, pos, alpha=0.5, edge_color='#d78a76',
+                           width=[2 + 6 * edge_weights[edge] / max_edge_weight for edge in graph.edges()])
+    nx.draw_networkx_nodes(graph, pos, node_color='#e70e02', node_size=130, alpha=0.55)
+    node_labels = {node: " ".join(node) for node in graph.nodes()}
+    nx.draw_networkx_labels(graph, pos, labels=node_labels, font_size=18, font_color='black', font_family='sans-serif',
+                            alpha=1)
+    plt.axis('off')
+    return plt.gcf()  # Return the figure object
+
+# Streamlit app code
+page_bg_img = '''
+<style>
+
+header{visibility: hidden;}
+.stAlert{ visibility: hidden; }
+.stApp { background: url(http://localhost:3000/static/media/chart-back.4125ee2c.svg), no-repeat;}
+</style>
+'''
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+initial_min_frequency = 11
+min_frequency = st.slider('Minimum Frequency', 1, 20, initial_min_frequency)
+
+# Generate the graph
+with st.spinner():
+    graph = generate_graph(min_frequency)
+
+# Display the graph using st.pyplot
+    st.pyplot(graph)
+    st.success('success')
