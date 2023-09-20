2926f49d9b47cb658284ff4f169b782cd4929b74
Aryan Sadeghi <aryan.sadeghi@snapp.cab>
2023-09-20 20:31:37 +0330

    border radius

--- src/Views/Pages/Home/Sections/Chat/index.tsx
+++ src/Views/Pages/Home/Sections/Chat/index.tsx
@@ -29,7 +29,7 @@ const Chat = () => {
             <div style={{display:  (isOpen? "block" : "none" )}} className="chat-popup" id="myForm">
             <div className="form-container">
                 <iframe id="serviceFrameSend"
-                        style={{height: '620px', width:'100%'}}	src="http://178.128.236.200:7861/"
+                        style={{height: '620px', width:'100%',borderRadius:"10px"}}	src="http://178.128.236.200:7861/"
                         frameBorder="0"></iframe>
                 <button type="button" className="btn cancel" onClick={closeForm}>Close</button>
             </div>
